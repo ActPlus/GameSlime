d4303f7f8d458f02a0c36698f3f09fd090b70cf1
ActPlus <kikprofik@gmail.com>
2017-01-26 16:53:19 +0100

    bla

---
+++ android/src/sk/actplus/slime/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package sk.actplus.slime;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import sk.actplus.slime.GameSlime;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new GameSlime(), config);
+	}
+}
---
+++ desktop/src/sk/actplus/slime/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package sk.actplus.slime.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import sk.actplus.slime.GameSlime;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GameSlime(), config);
+	}
+}
