0d386553135c726ec9b9fb2960bda6a751aa9798
ActPlus <kikprofik@gmail.com>
2017-01-26 20:36:58 +0100

    copy

--- core/src/sk/actplus/slime/GameSlime.java
+++ core/src/sk/actplus/slime/GameSlime.java
@@ -10,20 +10,17 @@ public class GameSlime extends ApplicationAdapter {
 	SpriteBatch batch;
 	Texture img;
   
-  int kentus;
+
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
 	}
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
-		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		batch.begin();
-		batch.draw(img, 0, 0);
+		
 		batch.end();
 	}
 	
