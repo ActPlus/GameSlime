8f9f7df03ff6da4cfb340a4e85db7354a670090e
Kirill Kuznecov <kikprofik@gmail.com>
2018-02-20 07:26:45 +0100

    Version 2.1.1 Map Generator implementation map generator have bugs

---
+++ core/src/sk/actplus/slime/version2/GameArray.java
@@ -0,0 +1,14 @@
+package sk.actplus.slime.version2;
+
+import com.badlogic.gdx.utils.Array;
+
+import sk.actplus.slime.version2.entity.EntityArray;
+import sk.actplus.slime.version2.entity.mapentity.Triangle;
+
+/**
+ * Created by Ja on 19.2.2018.
+ */
+
+public class GameArray {
+    Array<Triangle> triangles = new Array<Triangle>();
+}
---
+++ core/src/sk/actplus/slime/version2/gui/GameOverGUI.java
@@ -0,0 +1,17 @@
+package sk.actplus.slime.version2.gui;
+
+/**
+ * Created by Ja on 18.2.2018.
+ */
+
+public class GameOverGUI extends GUI{
+    @Override
+    public void create() {
+
+    }
+
+    @Override
+    public void validate() {
+
+    }
+}
